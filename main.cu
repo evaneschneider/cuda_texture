21f35629df4f5e9b666378c78c2313efdebf61e6
Evan Schneider <evanelizabeth88@gmail.com>
2016-02-15 09:52:09 -0700

    working version of cholla cooling

--- main.cu
+++ main.cu
@@ -1,72 +1,91 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<sys/time.h>
+
 
 
 
 // Simple transformation kernel
-__global__ void transformKernel(float* output, cudaTextureObject_t texObj, int nx, int ny)
+__global__ void transformKernel(float* output, cudaTextureObject_t coolTexObj, cudaTextureObject_t heatTexObj, int nx, int ny, float log_n)
 {
   // Calculate normalized texture coordinates
   int xid = blockIdx.x * blockDim.x + threadIdx.x;
   int yid = blockIdx.y * blockDim.y + threadIdx.y;
-  float x = (xid+0.5f)/nx;
-  float y = (yid+0.5f)/ny;
+  float t = float(xid)/nx;
+  float n = (log_n + 6.0)/12.0 - 0.5/121;
   if (xid < nx && yid < ny) {
-    //printf("%f %f\n", x, y);
+    //printf("%3d %d %f %f\n", xid, yid, n, t);
     // Read from texture and write to global memory
-    output[yid * nx + xid] = tex2D<float>(texObj, x, y);
+    output[yid*nx + xid] = tex2D<float>(coolTexObj, t, n);
+    output[nx*ny + yid*nx + xid] = tex2D<float>(heatTexObj, t, n);
   }
 }
+
+void Load_Cooling_Tables(float* cooling_table, float* heating_table);
+
+double get_time(void);
 
 
 // Host code
 int main()
 {
-  float *h_data;
+  float *cooling_table;
+  float *heating_table;
   float *h_output;
-  const int nx = 5;
-  const int ny = 5;
-  int nx_out = 10;
-  int ny_out = 10; 
+  const int nx = 41;
+  const int ny = 121;
+  int nx_out = 1000000;
+  int ny_out = 1; 
+  float log_n = 0.0;
+  double start_t, stop_t;
 
 
-  // allocate table on host
-  h_data = (float *) malloc(nx*ny*sizeof(float));
-  for (int j=0; j<ny; j++) {
-    for (int i=0; i<nx; i++) {
-      h_data[i+nx*j] = (i*nx+j*ny)/nx;
-      printf("%5.2f ", h_data[i+nx*j]);
-    }
-    printf("\n");
-  }
+  // allocate arrays to be copied to textures
+  cooling_table = (float *) malloc(nx*ny*sizeof(float));
+  heating_table = (float *) malloc(nx*ny*sizeof(float));
+
+  // Load cooling table into the array
+  Load_Cooling_Tables(cooling_table, heating_table);
 
   // allocate output array on host
-  h_output = (float *) malloc(nx_out*ny_out*sizeof(float));
+  h_output = (float *) malloc(2*nx_out*ny_out*sizeof(float));
   for (int i=0; i<nx_out; i++) {
     for (int j=0; j<ny_out; j++) {
       h_output[i+nx_out*j] = 0.0;
     }
   }
- 
+
+  // Allocate array to store result of transformations in device memory
+  float* output;
+  cudaMalloc(&output, 2*nx_out*ny_out*sizeof(float));
+
   // set info for cuda kernels
   dim3 dimBlock(16, 16);
-  dim3 dimGrid((nx + dimBlock.x - 1) / dimBlock.x, (ny + dimBlock.y - 1) / dimBlock.y, 1);
+  dim3 dimGrid((nx_out + dimBlock.x - 1) / dimBlock.x, (ny_out + dimBlock.y - 1) / dimBlock.y, 1);
 
 
   // Allocate CUDA array in device memory
   cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
-  cudaArray* cuArray;
-  cudaMallocArray(&cuArray, &channelDesc, nx, ny);
-  // Copy to device memory some data located at address h_data
+  cudaArray* cuCoolArray;
+  cudaArray* cuHeatArray;
+  cudaMallocArray(&cuCoolArray, &channelDesc, nx, ny);
+  cudaMallocArray(&cuHeatArray, &channelDesc, nx, ny);
+  // Copy to device memory the cooling and heating arrays
   // in host memory
-  cudaMemcpyToArray(cuArray, 0, 0, h_data, nx*ny*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpyToArray(cuCoolArray, 0, 0, cooling_table, nx*ny*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpyToArray(cuHeatArray, 0, 0, heating_table, nx*ny*sizeof(float), cudaMemcpyHostToDevice);
 
-  // Specify texture
-  struct cudaResourceDesc resDesc;
-  memset(&resDesc, 0, sizeof(resDesc));
-  resDesc.resType = cudaResourceTypeArray;
-  resDesc.res.array.array = cuArray;
+  // Specify textures
+  struct cudaResourceDesc coolResDesc;
+  memset(&coolResDesc, 0, sizeof(coolResDesc));
+  coolResDesc.resType = cudaResourceTypeArray;
+  coolResDesc.res.array.array = cuCoolArray;
+  struct cudaResourceDesc heatResDesc;
+  memset(&heatResDesc, 0, sizeof(heatResDesc));
+  heatResDesc.resType = cudaResourceTypeArray;
+  heatResDesc.res.array.array = cuHeatArray;  
 
-  // Specify texture object parameters
+  // Specify texture object parameters (same for both tables)
   struct cudaTextureDesc texDesc;
   memset(&texDesc, 0, sizeof(texDesc));
   texDesc.addressMode[0] = cudaAddressModeClamp; // out-of-bounds fetches return border values
@@ -75,38 +94,163 @@ int main()
   texDesc.readMode = cudaReadModeElementType;
   texDesc.normalizedCoords = 1;
 
-  // Create texture object
-  cudaTextureObject_t texObj = 0;
-  cudaCreateTextureObject(&texObj, &resDesc, &texDesc, NULL);
-
-  // Allocate result of transformation in device memory
-  float* output;
-  cudaMalloc(&output, nx_out * ny_out * sizeof(float));
+  // Create texture objects
+  cudaTextureObject_t coolTexObj = 0;
+  cudaCreateTextureObject(&coolTexObj, &coolResDesc, &texDesc, NULL);
+  cudaTextureObject_t heatTexObj = 0;
+  cudaCreateTextureObject(&heatTexObj, &heatResDesc, &texDesc, NULL);
 
   // Invoke kernel
-  printf("About to perform texture read...\n");
-  transformKernel<<<dimGrid, dimBlock>>>(output, texObj, nx_out, ny_out);
+  start_t = get_time();  
+  transformKernel<<<dimGrid, dimBlock>>>(output, coolTexObj, heatTexObj, nx_out, ny_out, log_n);
+  stop_t = get_time();
+  printf("%f ms\n", (stop_t-start_t)*1000);  
   cudaDeviceSynchronize();
-  printf("Texture read complete.\n");
 
-  cudaMemcpy(h_output, output, nx_out*ny_out*sizeof(float), cudaMemcpyDeviceToHost);
+  // Copy the results back to the host
+  cudaMemcpy(h_output, output, 2*nx_out*ny_out*sizeof(float), cudaMemcpyDeviceToHost);
+
 
   for (int j=0; j<ny_out; j++) {
     for (int i=0; i<nx_out; i++) {
-      printf("%5.2f ", h_output[i+nx_out*j]);
+      //printf("%6.3f %6.3f\n", h_output[j*nx_out + i], h_output[nx_out*ny_out + j*nx_out + i]);
     }
-    printf("\n");
+    //printf("\n");
   }  
 
+
   // Destroy texture object
-  cudaDestroyTextureObject(texObj);
+  cudaDestroyTextureObject(coolTexObj);
+  cudaDestroyTextureObject(heatTexObj);
   // Free device memory
-  cudaFreeArray(cuArray);
+  cudaFreeArray(cuCoolArray);
+  cudaFreeArray(cuHeatArray);
   cudaFree(output);
   // Free host memory
-  free(h_data);
+  free(cooling_table);
+  free(heating_table);
   free(h_output);
 
   return 0;
 
+}
+
+
+
+void Load_Cooling_Tables(float* cooling_table, float* heating_table)
+{
+  double *n_arr;
+  double *T_arr;
+  double *L_arr;
+  double *H_arr;
+
+  int i;
+  int nx = 121;
+  int ny = 41;
+
+  FILE *infile;
+  char buffer[0x1000];
+  char * pch;
+
+  // Allocate arrays for high temperature data
+  n_arr = (double *) malloc(61*sizeof(double));
+  T_arr = (double *) malloc(61*sizeof(double));
+  L_arr = (double *) malloc(61*sizeof(double));
+  H_arr = (double *) malloc(61*sizeof(double));
+
+  // Read in high T cooling curve (single density)
+  i=0;
+  infile = fopen("./cloudy_coolingcurve_highT.txt", "r");
+  if (infile == NULL) {
+    printf("Unable to open Cloudy file.\n");
+    exit(1);
+  }
+  while (fgets(buffer, sizeof(buffer), infile) != NULL)
+  {
+    if (buffer[0] == '#') {
+      continue;
+    }
+    else {
+      pch = strtok(buffer, "\t");
+      n_arr[i] = atof(pch);
+      while (pch != NULL)
+      {
+        pch = strtok(NULL, "\t");
+        if (pch != NULL)
+          T_arr[i] = atof(pch);
+        pch = strtok(NULL, "\t");
+        if (pch != NULL)
+          L_arr[i] = atof(pch);
+        pch = strtok(NULL, "\t");
+        if (pch != NULL)
+          H_arr[i] = atof(pch);
+      }
+      i++;
+    }
+  }
+  fclose(infile);
+
+  free(n_arr);
+  free(T_arr);
+  free(L_arr);
+  free(H_arr);
+
+  // allocate arrays for low temperature data
+  n_arr = (double *) malloc(nx*ny*sizeof(double));
+  T_arr = (double *) malloc(nx*ny*sizeof(double));
+  L_arr = (double *) malloc(nx*ny*sizeof(double));
+  H_arr = (double *) malloc(nx*ny*sizeof(double));
+
+  // Read in low T cooling curve (function of density and temperature)
+  i=0;
+  infile = fopen("./cloudy_coolingcurve_lowT.txt", "r");
+  if (infile == NULL) {
+    printf("Unable to open Cloudy file.\n");
+    exit(1);
+  }
+  while (fgets(buffer, sizeof(buffer), infile) != NULL)
+  {
+    if (buffer[0] == '#') {
+      continue;
+    }
+    else {
+      pch = strtok(buffer, "\t");
+      n_arr[i] = atof(pch);
+      while (pch != NULL)
+      {
+        pch = strtok(NULL, "\t");
+        if (pch != NULL)
+          T_arr[i] = atof(pch);
+        pch = strtok(NULL, "\t");
+        if (pch != NULL)
+          L_arr[i] = atof(pch);
+        pch = strtok(NULL, "\t");
+        if (pch != NULL)
+          H_arr[i] = atof(pch);
+      }
+      i++;
+    }
+  }
+  fclose(infile);
+
+  // copy data from cooling array into the table
+  for (i=0; i<nx*ny; i++)
+  {
+    cooling_table[i] = float(L_arr[i]);
+    heating_table[i] = float(H_arr[i]);
+  }
+
+  // Free arrays used to read in table data
+  free(n_arr);
+  free(T_arr);
+  free(L_arr);
+  free(H_arr);
+}
+
+
+double get_time(void)
+{ 
+  struct timeval timer;
+  gettimeofday(&timer,NULL);
+  return timer.tv_sec + 1.0e-6*timer.tv_usec;
 }
---
+++ plot_coolingcurve.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+T = np.linspace(1.0, 5.0, 101)
+
+x_i = []
+y_i = []
+z_i = []
+f = open('cloudy_coolingcurve_n0_HM05.txt', 'r')
+f.readline()
+lines = f.readlines()
+f.close()
+for line in lines:
+  p = line.split()
+  x_i.append(float(p[1]))
+  y_i.append(float(p[2]))
+  z_i.append(float(p[3]))
+x = np.array(x_i)
+y = np.array(y_i)
+z = np.array(z_i)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+line1, = ax1.plot(x, y, 'bo', label="Cloudy data")
+line2, = ax1.plot(x, z, 'ro', label="Cloudy data")
+plt.xlabel("log(T) [K]");
+plt.ylabel("log($\Lambda$ / n$_{h}$$^{2}$) [erg cm$^{-3}$ s$^{-1}$]");
+
+
+x_i = []
+y_i = []
+f = open('cuda_coolingcurve_n0.txt', 'r')
+lines = f.readlines()
+f.close()
+for line in lines:
+  p = line.split()
+  x_i.append(float(p[0]))
+  y_i.append(float(p[1]))
+x = np.array(x_i)
+y = np.array(y_i)
+
+line3, = ax1.plot(T, x, color="green", label="2D interpolation")
+line4, = ax1.plot(T, y, color="green")
+
+
+plt.legend(handles=[line1, line3], loc=2)
+plt.show()
+fig.savefig('cuda_coolingcurve_n0.png')
